e454dc5dab7a0e3a22b85cb4d842aed2fcb1859d
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-09-16 15:33:46 +0200

    first take on gravity

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
 var bird = document.getElementById("bird")
 var height = 0
-var left = 0
 var right = 0
+var array = {}
+var up = true
+var grav = 0
 
-
-document.addEventListener("keypress", movement);
-
-function movement(key) {
-    if(key.code == "KeyW"){
-        height = height + 10
-        bird.style.bottom = height + "px"
+onkeydown = onkeyup = async function(key){
+    array[key.keyCode] = key.type == "keydown";
+    if(array[87] && up === true){  
+        grav = 0
+        height = height + 30
+        bird.style.bottom = height + "px" 
+        up = false
+        await sleep(100)
+        gravity()
+        await sleep(500)
+        up = true
     }
-    if(key.code == "KeyD"){
-        left = left + 10
-        bird.style.left = left + "px"
-    }   
-    if(key.code == "KeyA"){
-        left = left - 10
-        bird.style.left = left + "px"
-    }   
+
+    if(array[87] && array[68] || array[68]){
+        right = right + 10
+        bird.style.left = right + "px"
+    }
+
+    if(array[87] && array[65] || array[65]){
+        right = right - 10
+        bird.style.left = right + "px"
+    }
+    
+}
+
+async function gravity(){
+    console.log(parseInt(bird.style.bottom))
+    for(grav; grav < 9.81; grav = grav + 0.01){
+        if (array[87]) {        
+            grav = 0 
+            console.log(grav)
+        }
+        height = height - grav
+        bird.style.bottom = height + "px" 
+        await sleep(50)
+    }
+}
+
+function sleep(time) {      //Functie om pauzes te maken
+    return new Promise(resolve => setTimeout(resolve, time));
 }
