0b9171335b1a126a1de183e75f201c965574786a
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-09-21 14:33:30 +0200

    controls screen

--- main.js
+++ main.js
@@ -10,6 +10,24 @@ var b1 = document.getElementById("Ebutton")
 var b2 = document.getElementById("Mbutton")
 var b3 = document.getElementById("Jbutton")
 var heightH = document.getElementById("heightH")
+var startB = document.getElementById("startButton")
+var controls = document.getElementById("controls")
+var icon = document.getElementById("icon")
+
+startB.onclick = async function(){
+    controls.style.transition = "2s"
+    controls.style.left = "100vw"
+    await sleep(2000)
+    controls.style.transition = "0s"
+}
+
+icon.onclick = async function(){
+    controls.style.transition = "2s"
+    controls.style.left = "0"
+    await sleep(2000)
+    controls.style.transition = "0s"
+}
+
 
 b1.onclick = function(){    //verander achtergrond en gravity per button
     gravity = 100 
@@ -18,45 +36,45 @@ b1.onclick = function(){    //verander achtergrond en gravity per button
     wing.style.backgroundImage = "url(sprites/vleugel.png)"
 
 }
-b2.onclick = function(){
+b2.onclick = function(){    //verander achtergrond en gravity per button
     gravity = 200 
     document.body.style.backgroundImage = "url(sprites/maan.jpg)"
     document.body.style.backgroundPosition = "bottom"
     bird.style.backgroundImage =  "url(sprites/ruimteeend.png)"
     wing.style.backgroundImage = "url(sprites/vleugel.png)"
 }
-b3.onclick = function(){
+b3.onclick = function(){    //verander achtergrond en gravity per button
     gravity = 50 
     document.body.style.backgroundImage = "url(sprites/jupiter.jpg)"
     bird.style.backgroundImage =  "url(sprites/alieneend.png)"
     wing.style.backgroundImage = "url(sprites/aliendvleugel.png)"
 }
 
-slider1.oninput = function wingSpan(){
+slider1.oninput = function wingSpan(){  //verander de grootte van de vleugel en hoe snel de vogel omhoog gaat met de slider
     isSettingChanging = true
     document.documentElement.style.setProperty("--var-transform", "scale(" + slider1.value / 50 + ")")
-    wing.style.transform = "scale(" + slider1.value / 50 + ") rotate(30deg)"}
+    wing.style.transform = "scale(" + slider1.value / 50 + ") rotate(30deg)"
+}
 
-slider2.oninput = function birdSize(){
+slider2.oninput = function birdSize(){  //verander de grootte van de vogel met de slider
     isSettingChanging = true
     bird.style.transform = "scale(" + slider2.value / 50 + ")"
 }
 
-slider1.onmouseup = slider2.onmouseup = function settingsNotChanging(){
+slider1.onmouseup = slider2.onmouseup = function settingsNotChanging(){     //detect wanneer de slider niet word gebruikt
     isSettingChanging = false
 }
 
 checkHeight()
+checkIfIdle()
 
-
-onkeydown = onkeyup = async function(key){
+onkeydown = onkeyup = async function(key){      //functie om te kijken welke knoppen worden ingedrukt
     array[key.keyCode] = key.type == "keydown"
-    if(array[87] && up === true){  
+    if(array[87] && up === true || array[38] && up === true){   //als de W knop / pijl omhoog word ingedrukt
         bird.style.transition = "bottom " + 0.3 + "s ease-out"
         height = parseInt(getComputedStyle(bird).bottom) + slider1.value / 1.5 * gravity / 100
         bird.style.bottom = height + "px" 
         up = false
-        checkIfIdle()
         animateWing()
         await sleep(500)
         bird.style.transition = "bottom " + height * gravity / 5000 + "s ease-out"
@@ -64,7 +82,7 @@ onkeydown = onkeyup = async function(key){
         up = true
     }
 
-    if(array[87] && array[68] || array[68]){
+    if(array[87] && array[68] || array[68] || array[38] && array[39] || array[39]){    //als de D knop / rechter pijl word ingedrukt of als de D knop / rechter pijl en de W knop / pijl omhoog worden ingedrukt
         right = right + 5
         bird.style.left = right + "px"
         bird.style.transform = "scaleX(1) scale(" + slider2.value / 50 + ")"
@@ -73,7 +91,7 @@ onkeydown = onkeyup = async function(key){
         }
     }
 
-    if(array[87] && array[65] || array[65]){
+    if(array[87] && array[65] || array[65] || array[38] && array[37] || array[37]){    //als de A knop / rechter pijl word ingedrukt of als de A knop / rechter pijl en de W knop / pijl omhoog worden ingedrukt
         right = right - 5
         bird.style.left = right + "px"
         bird.style.transform = "scaleX(-1) scale(" + slider2.value / 50 + ")"
@@ -83,20 +101,20 @@ onkeydown = onkeyup = async function(key){
     }
 }
 
-async function checkHeight(){
-    heightH.innerHTML = "height: " + parseInt(getComputedStyle(bird).bottom) / 40 + "m"
+async function checkHeight(){   //checkt de hoogte van de vogel
+    heightH.innerHTML = "height: " + Math.round(parseInt(getComputedStyle(bird).bottom) / 40 * 10) / 10 + "m"
     await sleep(100)
     checkHeight()
 }
 
-async function animateWing(){
+async function animateWing(){   //animeert de vleugel als de vogel omhoog gaat
     wing.style.animation = "flap 0.5s"
     await sleep(500)
     wing.style.animation = ""
 }
 
-async function checkIfIdle(){
-    if(array[87] === false && isSettingChanging === false){    
+async function checkIfIdle(){   //kijkt of de vogel aan het vallen is en geeft dan een animatie aan de vleugel
+    if(array[87] === false && array[38] === undefined && isSettingChanging === false || array[87] === undefined && array[38] === false && isSettingChanging === false || array[87] === false && array[38] === false && isSettingChanging === false){    
         if(parseInt(getComputedStyle(bird).bottom) > 0){
             wing.style.animation = "idle 0.2s infinite"
         }
@@ -108,6 +126,6 @@ async function checkIfIdle(){
     checkIfIdle()
 }
 
-function sleep(time) {      //Functie om pauzes te maken
-    return new Promise(resolve => setTimeout(resolve, time));
+function sleep(time) {      //functie om pauzes te maken
+    return new Promise(resolve => setTimeout(resolve, time))
 }
